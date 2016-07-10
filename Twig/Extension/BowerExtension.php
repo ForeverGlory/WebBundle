9637352c3dbd82ae835750e975f16382d57426db
ForeverGlory <foreverglory@qq.com>
2016-07-10 15:48:27 +0800

    {{bower_path()}} 获取 bower 的地址，默认

---
+++ Twig/Extension/BowerExtension.php
@@ -0,0 +1,55 @@
+<?php
+
+/*
+ * This file is part of the current project.
+ * 
+ * (c) ForeverGlory <http://foreverglory.me/>
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Glory\Bundle\WebBundle\Twig\Extension;
+
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Description of BowerExtension
+ *
+ * @author ForeverGlory <foreverglory@qq.com>
+ */
+class BowerExtension extends \Twig_Extension
+{
+
+    /**
+     * @var ContainerInterface 
+     */
+    protected $container;
+    protected $path;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
+    public function getFunctions()
+    {
+        return [
+            new \Twig_SimpleFunction('bower_path', [$this, 'getPath'])
+        ];
+    }
+
+    public function getPath()
+    {
+        if (!$this->path) {
+            $this->path = 'bower_componets';
+        }
+        return $this->path;
+    }
+
+    public function getName()
+    {
+        return 'glory_web.bower';
+    }
+
+}
